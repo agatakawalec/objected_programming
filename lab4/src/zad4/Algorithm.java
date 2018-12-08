f16cd42f0c1a4e9234b3fa04cac662ece74b44ed
agatakawalec <kawalecagata98@gmail.com>
2018-12-08 18:35:21 +0100

    deleting content of lab4

--- lab4/src/zad4/Algorithm.java
+++
@@ -1,6 +0,0 @@
-package zad4;
-
-interface Algorithm {
-    StringBuilder crypt(StringBuilder word);
-    StringBuilder encrypt(StringBuilder word);
-}
--- lab4/src/zad4/Crypto_main.java
+++
@@ -1,7 +0,0 @@
-package zad4;
-
-public class Crypto_main {
-    public static void main(String[] argv){
-        System.out.print("Hello World");
-    }
-}
