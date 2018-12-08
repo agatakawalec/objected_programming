b7f4248cb1a3cceb59ea51d73b349b467645db2a
agatakawalec <kawalecagata98@gmail.com>
2018-12-08 19:08:03 +0100

    Creating new order on my git

---
+++ lab2/src/DataFrame/DataFrame.java
@@ -0,0 +1,11 @@
+package DataFrame;
+
+import java.util.ArrayList;
+
+public class DataFrame<T> {
+    private ArrayList<ArrayList> frame;
+    DataFrame(String[] a, String[] b){
+
+        //ArrayList<int> a[0]= new ArrayList<int>();
+    }
+}
